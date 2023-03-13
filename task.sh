9d5bbfead615b2476f09002861d7c70c6a400ed7
Tomáš Hobza <thomashobza@ids.company>
2023-03-13 21:48:19 +0100

    add: list

--- task.sh
+++ task.sh
@@ -11,6 +11,8 @@ m=false
 is_file=false
 is_directory=false
 path=''
+a=''
+b=''
 
 usage() {
   echo 'USAGE'
@@ -74,12 +76,6 @@ fi
 ### parsing arguments ###
 #########################
 
-# no arguments were provided
-if [ $# -eq 0 ]; then
-  usage
-  exit 0
-fi
-
 # -h argument resolves to just displaying usage
 if [ "$1" == "-h" ]; then
   usage
@@ -99,15 +95,21 @@ if [ "$1" = "secret-log" ]; then
 fi
 
 # parse other arguments with -<opt. letter> <value>? format
-while getopts ":g:m" opt; do
+while getopts ":g:ma:b:" opt; do
   case $opt in
   g)
     # -g was provided
-    group=$OPTARG
+    group="$OPTARG"
     ;;
   m)
     # -m was provided
     m=true
+    ;;
+  a)
+    a="$OPTARG"
+    ;;
+  b)
+    b="$OPTARG"
     ;;
   \?)
     # invalid option handle
@@ -129,24 +131,22 @@ done
 # remove arguments from argument array
 shift $((OPTIND - 1))
 
-# no unparsed arguments were left -> no file/dir was provided
-if [ "$*" = "" ]; then
-  echo "error: An input file/directory must be provided."
-  echo
-  usage
-  exit 1
-fi
-
 # only argument left should be the file/directory
-if [ $# -ne 1 ]; then
+if [ $# -gt 1 ]; then
   echo "Too many arguments!"
   echo
   usage
   exit 1
 fi
 
+# check for empty input -> current directory
+input="$*"
+if [ -z "$input" ]; then
+  input='./'
+fi
+
 # check whether input was a file or a directory
-if [ -f "$*" ]; then
+if [ -f "$input" ]; then
   # -m is not allowed with FILE parameter
   if $m; then
     echo "Illegal option '-m' for opening FILE."
@@ -157,45 +157,132 @@ if [ -f "$*" ]; then
 
   # input is file
   is_file=true
-  path="$*"
-elif [ -d "$*" ]; then
+  path="$input"
+elif [ -d "$input" ]; then
   # input is directory
   is_directory=true
-  path="$*"
+  path="$input"
 else
-  # input file/dir is non-existant
-  echo "'$*' is not a file nor a directory."
-  echo
-  usage
-  exit 1
+  # non-existant input
+  if [ "${input#${input%?}}" = '/' ]; then
+    # input dir is non-existant
+    echo "'$input' is not a directory."
+    exit 1
+  else
+    # input file is not existing yet
+    is_file=true
+    path="$input"
+  fi
 fi
+
+###############
+### listing ###
+###############
+
+if [ "$command" = "list" ]; then
+  # default arguments
+  args='$1 ~ path'
+
+  # for option -a
+  if [ -n "$a" ]; then
+    args=$args' && $2 > after'
+  fi
+
+  # for option -b
+  if [ -n "$b" ]; then
+    args=$args' && $2 < before'
+  fi
+
+  # for option -g
+  if [ -n "$group" ]; then
+    args=$args' && $4 == group'
+  fi
+
+  # add print command to arguments for awk
+  args=$args' {print $1}'
+
+  # find all unique paths to files based on set conditions
+  files="$(awk -v path="$path" -v after="$a" -v before="$b" -v group="$group" -F ';' "$args" "$MOLE_RC" | sort | uniq)"
+
+  # find the longest filepath
+  indent=0
+  for line in $files; do
+    len=${#line}
+    if [ "$len" -gt "$indent" ]; then
+      indent="$len"
+    fi
+  done
+
+  # write out every filepath with it's groups
+  echo "$files" | while IFS= read -r line; do
+    local_indent=$((indent - ${#line} + 1))
+    groups="$(awk -v path="$line" -F ';' '$1 ~ path && $4 {print $4}' "$MOLE_RC" | uniq | sort | tr '\n' ', ')"
+    printf "%s:%${local_indent}s%s\n" "$line" "" "${groups%?}"
+  done
+  exit
+fi
+
+################################
+### handling directory input ###
+################################
+
+if $is_directory; then
+  # default arguments
+  args='$1 ~ path'
+
+  # for option -a
+  if [ -n "$a" ]; then
+    args=$args' && $2 > after'
+  fi
+
+  # for option -b
+  if [ -n "$b" ]; then
+    args=$args' && $2 < before'
+  fi
+
+  # for option -g
+  if [ -n "$group" ]; then
+    args=$args' && $4 == group'
+  fi
+
+  # add print command to arguments for awk
+  args=$args' {print $1}'
+
+  line=''
+  if $m; then
+    # find the mostly occuring line based on set confition
+    line="$(awk -v path="$path" -v after="$a" -v before="$b" -v group="$group" -F ';' "$args" "$MOLE_RC" | awk '{ count[$0]++ } END { for(line in count) print line, count[line] }' | sort -k2nr | head -1 | awk '{print $1}')"
+  else
+    # find the last line in file based on set conditions
+    line="$(awk -v path="$path" -v after="$a" -v before="$b" -v group="$group" -F ';' "$args" "$MOLE_RC" | tail -n -1 | awk -F ';' '{print $1}')"
+  fi
+
+  # if no line was found show error message and exit with error
+  if [ -z "$line" ]; then
+    echo "No file has ever been opened in '$(realpath "$path")' with set parameters."
+    exit 1
+  fi
+
+  # overwrite the path to the now opening file
+  path="$line"
+  is_directory=false
+  is_file=true
+fi
+
+######################
+### opening editor ###
+######################
+
+# $editor "$path"
+mkdir -p "$(dirname "$path")" && touch "$path"
 
 ############################
 ### saving the open data ###
 ############################
 
 realpath="$(realpath "$path")"
+echo "otviram: $realpath"
 
 if $is_file; then
-  groupSuffix=''
-  if [ -n "$group" ]; then
-    groupSuffix=":$group"
-    echo $groupSuffix
-  fi
-  if [ -z "$(gsed -n "\;$realpath;p" "$MOLE_RC")" ]; then
-    # append new line record
-    echo "$realpath$groupSuffix;$(date "+%Y-%m-%d_%H-%M-%S")_1" >>"$MOLE_RC"
-  else
-    # calculate new record index
-    line=$(gsed -n "\;$realpath;p" "$MOLE_RC")
-    stripped=$(echo "$line" | tr -cd ';')
-    num=$(echo "$stripped" | wc -m)
-    num=$(($num))
-
-    sed -i "s;$realpath;$realpath:$groupSuffix;g" "$MOLE_RC"
-
-    # append to an existing record
-    # TODO: change gsed to sed
-    gsed -i "\;^$realpath; s/$/;$(date "+%Y-%m-%d_%H-%M-%S")_$num/" "$MOLE_RC"
-  fi
+  echo "$realpath;$(date "+%Y-%m-%d;%H-%M-%S");$group" >>"$MOLE_RC"
 fi
